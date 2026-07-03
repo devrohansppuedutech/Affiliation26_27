27208594779b95ab334559a7ccb09e16108a1bcb
ketan <ketan.official123@gmail.com>
2026-07-03 13:48:54 +0530

    EMP changes 03/072026.1218

--- src/areas/Employee/services/employeeService.js
+++ src/areas/Employee/services/employeeService.js
@@ -9,3 +9,20 @@ export const getEmployees = async () => {
 
   return await response.json();
 };
+
+import axios from "axios";
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export const getEmployees = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/employees";
+
+export const getEmployees = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
